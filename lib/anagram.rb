0aa668156e55e337c8d428c55f6cbdd482f156ad
Daniel Lechok <dclechok@gmail.com>
2019-09-19 14:27:02 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,18 @@
 class Anagram
   
   attr_accessor :word
-  
+
   def initialize(new_word)
-    word = new_word
+		@word = new_word
   end
   
   def match(possible_anagram)
-
+		new_array = []
+		possible_anagram.each do |item|
+			if(@word.chars.sort == item.chars.sort)
+				new_array << item
+			end
+		end
+		new_array
   end
 end
